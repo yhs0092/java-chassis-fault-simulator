14de9d565c312eff3ec1fbd45c67bd5f66eaecf6
yhs0092 <yhs0092@163.com>
2019-03-13 23:34:47 +0800

    add provider project

---
+++ fault-simulator-provider/src/main/java/com/github/yhs0092/javachassis/demo/provider/ProviderApp.java
@@ -0,0 +1,15 @@
+package com.github.yhs0092.javachassis.demo.provider;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class ProviderApp {
+  public static void main(String[] args) throws Exception {
+    Log4jUtils.init();
+    BeanUtils.init();
+  }
+}
---
+++ fault-simulator-provider/src/main/java/com/github/yhs0092/javachassis/demo/provider/service/ProviderService.java
@@ -0,0 +1,17 @@
+package com.github.yhs0092.javachassis.demo.provider.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+import org.apache.servicecomb.provider.rest.common.RestSchema;
+
+@RestSchema(schemaId = "demo")
+@Path("provider/v0")
+public class ProviderService {
+  @Path("get")
+  @GET
+  public String get(@QueryParam("q") String q) {
+    return q;
+  }
+}
