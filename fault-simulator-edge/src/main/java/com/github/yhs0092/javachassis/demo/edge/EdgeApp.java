80dd66f56e42e0fa519d176a715a6200d46b4172
yaohaishi <yaohaishi@huawei.com>
2019-03-22 10:55:05 +0800

    edge: init

---
+++ fault-simulator-edge/src/main/java/com/github/yhs0092/javachassis/demo/edge/EdgeApp.java
@@ -0,0 +1,9 @@
+package com.github.yhs0092.javachassis.demo.edge;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+
+public class EdgeApp {
+  public static void main(String[] args) {
+    BeanUtils.init();
+  }
+}
