84d45aa85da26d3b382192946424d3efc2ec0229
yaohaishi <yaohaishi@huawei.com>
2019-04-18 20:50:52 +0800

    provider: add simulator operation

--- fault-simulator-provider/src/main/java/com/github/yhs0092/javachassis/demo/provider/service/ProviderService.java
+++ fault-simulator-provider/src/main/java/com/github/yhs0092/javachassis/demo/provider/service/ProviderService.java
@@ -1,22 +1,21 @@
 package com.github.yhs0092.javachassis.demo.provider.service;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
 
 import org.apache.servicecomb.provider.rest.common.RestSchema;
+
+import com.github.yhs0092.javachassis.demo.common.fault.simulation.FaultSimulationManager;
+import com.github.yhs0092.javachassis.demo.common.fault.simulation.FaultSimulationOrder;
+import com.github.yhs0092.javachassis.demo.common.fault.simulation.FramePosition;
 
 @RestSchema(schemaId = "demo")
 @Path("provider/v0")
 public class ProviderService {
-  @Path("get")
-  @GET
-  public String get(@QueryParam("q") String q) {
-    try {
-      Thread.sleep(1500);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-    return q;
+  @Path("simulate")
+  @POST
+  public String simulate(FaultSimulationOrder faultSimulationOrder) {
+    FaultSimulationManager.simulate(faultSimulationOrder, FramePosition.BIZ_BF);
+    return "OK";
   }
 }
